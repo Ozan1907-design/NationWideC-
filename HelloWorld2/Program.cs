4a4f87c68acb5e819697ce7edd5f3fd165493a13
Admin <Admin@MIS>
2025-10-20 11:22:27 +0100

    Add project files.

---
+++ HelloWorld2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorld2
+{
+    internal class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ HelloWorld3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HelloWorld2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String name = "Pete";
+            int[] arr =  new int[5];
+            for (int i = 0; i <name.Length; i++)
+            {
+                Console.WriteLine(name[i]);
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                arr[i] = i + 1;
+                Console.WriteLine(arr[i]);
+            }
+            Console.WriteLine("Hello World, my name is " + name);
+            
+        }
+    }
+}
+
